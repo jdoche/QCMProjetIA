8bb95f938c422b20c2652785b9fec6c9b92af39e
Colin Lashermes <clashermes@ensc.fr>
2018-11-06 12:00:19 +0100

    Initial commit

---
+++ QCM/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace QCM
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //stocker tout le monde dans une liste
+            //selectionner 20 question, selection de 20 fois une question
+            System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("iso-8859-1");
+            StreamReader monStreamReader = new StreamReader(@"Question.xml", encoding);
+            string ligne = monStreamReader.ReadLine();
+            int nombreLigne = int.Parse(ligne);//la première ligne du document texte comporte uniquement le nombre de question écrites
+
+            List<Question> listeQuestion;
+
+            for (int i = 0; i < nombreLigne; i++)
+            {
+                ligne = monStreamReader.ReadLine();
+                string[] intermediaire = ligne.Split(';'); //le caractère séparateur entre chaque information  est ";"
+
+                Q Question = new Question();//
+                Q = Question();
+              
+            }
+
+            monStreamReader.Close();
+
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ QCM/Question.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QCM
+{
+    class Question
+    {
+        protected string intitule;
+        protected List<string> listeReponse;
+        protected List<string> bonneReponse;
+        // création d'une question
+        Question()
+        { }
+
+        Question(string[] texte)
+        { intitule=texte[0];
+            
+           
+            for(int i=1;i<=texte.Length; i++ )
+            {
+                if(i<5)
+                {
+                    listeReponse.Add(texte[i]);
+                }
+                else
+                {
+                    bonneReponse.Add(texte[i]);
+                }
+
+            }
+        }
+
+
+    }
+}
